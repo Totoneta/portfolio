f36ae5a7d415401e10788c9b55af149d53a2401d
Tomás Godoy <tomas.godoy.litio@hotmail.com>
2025-01-01 03:41:33 -0300

    Cambios en datos de proyectos

--- portafoliotomasgodoy/src/data/proyectos.js
+++ portafoliotomasgodoy/src/data/proyectos.js
@@ -36,7 +36,7 @@ export let proyectos = [
         id: 104,
         titulo: 'Burguer&Chips',
         imgpath: './imgs/proyectos/burgerychipsproyecto.jpg',
-        enalceaproyecto: 'https://burguerychips.netlify.app/',
+        enalceaproyecto: 'https://burguerandchips.netlify.app/',
         tech: ['reactjs', 'css', 'redux'],
         responsive: false ,
     },
