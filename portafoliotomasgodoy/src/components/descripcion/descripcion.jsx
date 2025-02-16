00366f9c659bd1aa1a9e860e9c4d7ebe375a1624
Tomás Godoy <tomas.godoy.litio@hotmail.com>
2025-02-16 19:34:35 -0300

    descripcion actualizada

--- portafoliotomasgodoy/src/components/descripcion/descripcion.jsx
+++ portafoliotomasgodoy/src/components/descripcion/descripcion.jsx
@@ -5,7 +5,7 @@ export function Descripcion() {
     return (
         <div className="sobremisoycontainer">
             <h3>SOBRE MÍ</h3>
-            <span>¡Hola! Soy un apasionado de la informática y siempre busco mejorar de forma autodidacta. <strong>Mi especialidad es el desarrollo frontend</strong>, donde disfruto creando y reconstruyendo páginas web. Aunque no tengo experiencia laboral, <strong>manejo JavaScript, TypeScript, ReactJS y librerías de React</strong> para crear interfaces interactivas. También tengo conocimientos de <strong>backend, especialmente en Django, Django Rest Framework y JWT</strong>, aunque sigo mejorando en esa área. Mi objetivo es <strong>trabajar como programador en una empresa que me ofrezca estabilidad</strong> y me permita seguir desarrollando mis habilidades en frontend, tanto en aplicaciones web como móviles.</span>
+            <span>¡Hola! Soy un apasionado de la informática y siempre busco mejorar de forma autodidacta. <strong>Mi especialidad es el desarrollo frontend</strong>, donde disfruto creando y reconstruyendo páginas web. Cuento con experiencia laboral, <strong>manejo JavaScript, TypeScript, ReactJS y librerías de React(Axios, Redux, etc.)</strong> para crear interfaces interactivas. Tengo conocimientos de <strong>backend, especialmente en Django, Django Rest Framework, Corsheaders y JWT</strong>. Y cuento con conocimientos en <strong>testing, en tecnologías como Postman</strong>. aunque sigo mejorando en todas las áreas, soy eficaz y eficiente a la hora de contruir y/o rediseñar páginas.</span>
         </div>
     )
 }
